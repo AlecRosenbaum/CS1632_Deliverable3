63f0b0635a8b5ec123f51867fe51bd9452f4e113
thatdaxton <djs129@pitt.edu>
2017-10-23 10:07:50 -0400

    Added 2 tests
    Added in 2 text tests as well as fixed merge issues

--- WebTest.java
+++ WebTest.java
@@ -46,88 +46,33 @@ public class WebTest {
 		driver.get(baseURL);
 	}
 
-	// // Given that I am on the main page
-	// // When I view the title
-	// // Then I see that it contains the word "reddit"
-	// @Test
-	// public void testShowsCorrectTitle() {
+	// Given that I am on the main page
+	// Verify that the following text is present:
+	// "Welcome, friend, to a land of pure calculation"
+	// "Used for CS1632 Software Quality Assurance, taught by Bill Laboon"
+	@Test
+	public void testShowsCorrectHomePageText_000() {
+				
+		String bodyText = driver.findElement(By.className("lead")).getText();
+		System.out.println(bodyText);
+		assertTrue(bodyText.contains("Welcome, friend, to a land of pure calculation"));
+	}
 
-	// 	// Simply check that the title contains the word "reddit"
-
-	// 	String title = driver.getTitle();
-	// 	assertTrue(title.contains("reddit"));
-	// }
-
-	// // Given that I am on the main page
-	// // When I view the Remember Me section
-	// // Then I should see that it contains the phrase "remember me"
-	// @Test
-	// public void testHasRememberMe() {
-
-	// 	// Check that there is a remember-me element
-	// 	// that contains the text "remember me"
-	// 	// If it does not exist, or text is incorrect, fail test
-
-	// 	try {
-	// 		WebElement e = driver.findElement(By.id("remember-me"));
-	// 		String elementText = e.getText();
-	// 		assertTrue(elementText.contains("remember me"));
-	// 	} catch (NoSuchElementException nseex) {
-	// 		fail();
-	// 	}
-	// }
-
-	// // Given that I am on the main page
-	// // When I click on the "new" link
-	// // Then I should be redirected to the "new" page
-	// @Test
-	// public void testSeeNewLinks() {
-
-	// 	// find the "new" link and click on it
-	// 	// The page you go to should include "newest submissions"
-	// 	// in the title
-
-	// 	driver.findElement(By.linkText("new")).click();
-	// 	String newPageTitle = driver.getTitle();
-	// 	assertTrue(newPageTitle.contains("newest submissions"));
-	// }
-
-	// // Given that I am on the main page
-	// // And I am not logged in
-	// // When I try to login with an valid username and invalid password
-	// // Then I am given the opportunity to reset the password
-	// @Test
-	// public void testBadPasswordResetLink() {
-
-	// 	// Enter username "meow", password "meow"
-
-//                // ENTER YOUR OWN USERNAME/INVALID PASSWORD HERE!
-
-	// 	// driver.findElement(By.name("user")).sendKeys("dasbill");
-	// 	// driver.findElement(By.name("passwd")).sendKeys("meow");
-
-	// 	// Look for the submit button (in the login div) and click
-	// 	// to attempt to login
-
-	// 	WebElement loginDiv = driver.findElement(By.id("login_login-main"));
-
-	// 	WebElement submitButton = loginDiv.findElement(By.className("btn"));
-	// 	submitButton.click();
-
-	// 	// Check that there is a link to reset password and it is visible
-
-	// 	try {
-	// 		WebElement resetPw = driver.findElement(By.linkText("reset password"));
-	// 		assertTrue(resetPw.isDisplayed());
-	// 	} catch (NoSuchElementException nseex) {
-	// 		fail();
-	// 	}
-	// }
-
-	// The homepage shall display the text "Welcome, friend, to a land of pure calculation" as well as "Used for CS1632 Software Quality Assurance, taught by Bill Laboon"
-
-
-	// Every page shall include five links at the top, to "CS1632 D3 Home", "Factorial", "Fibonacci", "Hello", and "Cathedral Pics". These shall link to /, /fact, /fib, /hello, and /cathy, respectively.
+	// Given that I am on the main page
+	// Verify that the following text is present:
+	// "Welcome, friend, to a land of pure calculation"
+	// "Used for CS1632 Software Quality Assurance, taught by Bill Laboon"
+	@Test
+	public void testShowsCorrectHomePageText_001() {
+				
+		String bodyText = driver.findElement(By.className("lead")).getText();
+		System.out.println(bodyText);	
+		assertTrue(bodyText.contains("Used for CS1632 Software Quality Assurance, taught by Bill Laboon"));
+	}
+	
+	// Given that I am on the main page
+	// When I view the header
+	// Then I see that it contains "new", "rising", and "top" links
 	@Test
 	public void testHasCorrectHeaderLinks() {
 
