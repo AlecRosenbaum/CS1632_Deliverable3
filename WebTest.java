500d1b5cafb888b37b589081b3d65222913fd3ee
Alec Rosenbaum <alecr95@gmail.com>
2017-10-23 10:06:26 -0400

    no warning logging anymore

--- WebTest.java
+++ WebTest.java
@@ -1,24 +1,45 @@
 import static org.junit.Assert.*;
 
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.*;
 import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 
-/**
- * As a user,
- * I would like to see reddit links in all sorts of ways,
- * So that I can know what is happening in the world
- * @author wlaboon
- *
- */
+import java.util.logging.*;
+import org.openqa.selenium.logging.*;
+import org.openqa.selenium.remote.*;
+
 
 public class WebTest {
 
 	static WebDriver driver = new HtmlUnitDriver();
 	final static String baseURL = "https://cs1632ex.herokuapp.com/";
-	
+
+
+	// Note that the @BeforeClass annotation runs ONCE
+	// per class, before any of the test cases are run
+	// Compare this to @Before, which runs before
+	// EACH @Test-annotated method (test case)
+
+	@BeforeClass
+	public static void setUpDriver() {
+
+		// Note that the logging level is a Java standard (thus the
+		// use of a java.util class instead of something specific
+		// to Selenium.  You can modify these levels yourself if,
+		// for example, you would like to see only SEVERE errors.
+		// They can be set to ALL (show all messages) or OFF (no messages)
+		// or to any minimum level from FINEST (most verbose) to SEVERE
+		// (only show the most egregious of errors).
+		// A full lists of levels and descriptions are located at:
+		// https://docs.oracle.com/javase/7/docs/api/java/util/logging/Level.html
+
+		java.util.logging.Logger.getLogger("com.gargoylesoftware").setLevel(Level.OFF);
+		System.setProperty("org.apache.commons.logging.Log", "org.apache.commons.logging.impl.NoOpLog");
+		driver = new HtmlUnitDriver();
+
+	}
+
 	// Start at the home page for reddit for each test
 	@Before
 	public void setUp() throws Exception {
@@ -30,23 +51,23 @@ public class WebTest {
 	// // Then I see that it contains the word "reddit"
 	// @Test
 	// public void testShowsCorrectTitle() {
-		
+
 	// 	// Simply check that the title contains the word "reddit"
-		
+
 	// 	String title = driver.getTitle();
 	// 	assertTrue(title.contains("reddit"));
 	// }
-	
+
 	// // Given that I am on the main page
 	// // When I view the Remember Me section
 	// // Then I should see that it contains the phrase "remember me"
 	// @Test
 	// public void testHasRememberMe() {
-		
+
 	// 	// Check that there is a remember-me element
 	// 	// that contains the text "remember me"
 	// 	// If it does not exist, or text is incorrect, fail test
-		
+
 	// 	try {
 	// 		WebElement e = driver.findElement(By.id("remember-me"));
 	// 		String elementText = e.getText();
@@ -55,46 +76,46 @@ public class WebTest {
 	// 		fail();
 	// 	}
 	// }
-	
+
 	// // Given that I am on the main page
 	// // When I click on the "new" link
 	// // Then I should be redirected to the "new" page
 	// @Test
 	// public void testSeeNewLinks() {
-		
+
 	// 	// find the "new" link and click on it
 	// 	// The page you go to should include "newest submissions"
 	// 	// in the title
-		
+
 	// 	driver.findElement(By.linkText("new")).click();
 	// 	String newPageTitle = driver.getTitle();
 	// 	assertTrue(newPageTitle.contains("newest submissions"));
 	// }
-	
+
 	// // Given that I am on the main page
-	// // And I am not logged in 
+	// // And I am not logged in
 	// // When I try to login with an valid username and invalid password
 	// // Then I am given the opportunity to reset the password
 	// @Test
 	// public void testBadPasswordResetLink() {
-		
+
 	// 	// Enter username "meow", password "meow"
-	    
- //                // ENTER YOUR OWN USERNAME/INVALID PASSWORD HERE!
-	    
+
+//                // ENTER YOUR OWN USERNAME/INVALID PASSWORD HERE!
+
 	// 	// driver.findElement(By.name("user")).sendKeys("dasbill");
 	// 	// driver.findElement(By.name("passwd")).sendKeys("meow");
-		
+
 	// 	// Look for the submit button (in the login div) and click
-	// 	// to attempt to login 
-		
+	// 	// to attempt to login
+
 	// 	WebElement loginDiv = driver.findElement(By.id("login_login-main"));
-		
+
 	// 	WebElement submitButton = loginDiv.findElement(By.className("btn"));
 	// 	submitButton.click();
-		
+
 	// 	// Check that there is a link to reset password and it is visible
-		
+
 	// 	try {
 	// 		WebElement resetPw = driver.findElement(By.linkText("reset password"));
 	// 		assertTrue(resetPw.isDisplayed());
@@ -102,14 +123,14 @@ public class WebTest {
 	// 		fail();
 	// 	}
 	// }
-	
+
 	// The homepage shall display the text "Welcome, friend, to a land of pure calculation" as well as "Used for CS1632 Software Quality Assurance, taught by Bill Laboon"
-	
+
 
 	// Every page shall include five links at the top, to "CS1632 D3 Home", "Factorial", "Fibonacci", "Hello", and "Cathedral Pics". These shall link to /, /fact, /fib, /hello, and /cathy, respectively.
 	@Test
 	public void testHasCorrectHeaderLinks() {
-		
+
 		// Check for links + link location
 		try {
 			WebElement link = driver.findElement(By.linkText("CS1632 D3 Home"));
@@ -117,13 +138,13 @@ public class WebTest {
 
 			link = driver.findElement(By.linkText("Factorial"));
 			assertEquals(baseURL + "fact", link.getAttribute("href"));
-			
+
 			link = driver.findElement(By.linkText("Fibonacci"));
 			assertEquals(baseURL + "fib", link.getAttribute("href"));
-			
+
 			link = driver.findElement(By.linkText("Hello"));
 			assertEquals(baseURL + "hello", link.getAttribute("href"));
-			
+
 			link = driver.findElement(By.linkText("Cathedral Pics"));
 			assertEquals(baseURL + "cathy", link.getAttribute("href"));
 
@@ -133,15 +154,15 @@ public class WebTest {
 	}
 
 	// The factorial page (/fact) shall allow a user to enter a positive integer from 1 to 100, and upon pressing submit, shall show to the user the factorial of the value (e.g. "Factorial of 5 is 120!").
-	
+
 	// The Fibonacci page (/fib) shall allow a user to enter a positive integer from 1 to 100, and upon pressing submit, shall show to the user the Fibonnaci of the value (e.g. "Fibonacci of 5 is 8!").
-	
+
 	// For both the Fibonacci and Factorial pages, if a user enters an invalid value of any kind, they shall be informed that the value is 1 (e.g., "Fibonacci of -100 is 1!")
-	
+
 	// Accessing the hello page (/hello) with no trailing values in the URL shall display the message "Hello CS1632, from Prof. Laboon!".
-	
+
 	// If a trailing value is provided in the URL when accessing the /hello page, then the message shall display hello from that trailing value. For example, when accessing /hello/Jazzy, the system shall display "Hello CS1632, from Jazzy". This shall work for all input values.
-	
+
 	// The Cathedral page (/cathy) shall display three images of the Cathedral of Learning in a numbered list.
 
 }
